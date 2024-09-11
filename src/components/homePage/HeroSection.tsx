a5eff22fe7c9fc93b4ddc27e231b863c8acf2856
Michael Gitonga <gitoshmbae@gmail.com>
2024-09-11 13:11:10 +0300

    updates the homePage src/app/page.tsx layout and the homePage src/components/homePage/HeroSection.tsx

--- src/components/homePage/HeroSection.tsx
+++ src/components/homePage/HeroSection.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import { CldImage } from "next-cloudinary";
+import React from "react";
 
 export default function HeroSection() {
   return (
-    <section className="h-screen w-full grid place-content-center">
-        <h1 className='text-center uppercase font-bold text-6xl leading-[4.5rem] '>We are a <span className='bg-green-500 px-2'>talent solution</span> to <br/> the global tech demand</h1>
+    <section className="h-screen w-full grid place-content-center relative">
+      <CldImage
+        src="kosi-tech/about-us-hero-section/home-hero-bg.jpg_t8la6e"
+        height="900"
+        width="1400"
+        alt="kosi tech home hero bg"
+        className="object-cover h-full w-full absolute top-0 left-0  -z-20 "
+      />
+      <div className="absolute top-0 left-0 -z-10 h-screen w-screen bg-gradient-to-b  from-slate-100 via-transparent to-slate-100"></div>
+      <h1 className="text-center uppercase font-bold text-6xl leading-[4.5rem]  z-0">
+        We are a <span className="bg-green-500 px-2">talent solution</span> to{" "}
+        <br /> the global tech demand
+      </h1>
     </section>
-  )
+  );
 }
