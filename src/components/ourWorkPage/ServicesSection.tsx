bf8555db632d5709c63195f74eb416fa2c00a554
Michael Gitonga <gitoshmbae@gmail.com>
2024-09-11 17:32:49 +0300

    updates ourWork page sections to be more reponsives across devices, updates src/components/homePage/ReadyCTASection.tsx to have a different background base on the url

--- src/components/ourWorkPage/ServicesSection.tsx
+++ src/components/ourWorkPage/ServicesSection.tsx
@@ -5,23 +5,24 @@ import { CldImage } from "next-cloudinary";
 
 export default function ServicesSection() {
   return (
-    <section className="h-full w-full px-4  ">
+    // <section className="h-full w-full px-4  mt-20">
+    <section className="h-full w-full container mx-auto text-white px-4  pt-20">
       <div className="max-w-7xl mx-auto grid place-content-center">
-        <h2 className="uppercase text-2xl mb-8">
+        <h2 className="uppercase text-xl lg:text-2xl mb-8">
           {" "}
           <span className="text-yellow-400 uppercase">&#8226;</span>Services
         </h2>
-        <p className="text-5xl leading-[3.5rem]">
+        <p className="text-3xl lg:text-5xl leading-[2.5rem] lg:leading-[3.5rem]">
           {" "}
           Unlock your brand potential and drive innovation with our expert
           solutions.{" "}
         </p>
       </div>
-      <div className="space-y-12 mt-20">
+      <div className="space-y-12 py-20">
         {servicesCard.map((service) => (
           <div
             key={service.id}
-            className="flex flex-col md:flex-row gap-8 py-4 px-8 justify-between items-center  bg-gray-400 text-white"
+            className="flex flex-col md:flex-row gap-8 py-4 px-8 justify-between items-center  bg-gray-700 text-white"
           >
             <div className="flex flex-col w-full md:w-5/12">
               <p className="text-lg">{service.number}.</p>
