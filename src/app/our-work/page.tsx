767971c916d49c637e7ca7ad761c6564ce889b45
Michael Gitonga <gitoshmbae@gmail.com>
2024-09-12 15:18:29 +0300

    modifys the src/components/ourWorkPage/ServicesSection.tsx so that the sections stack on one another

--- src/app/our-work/page.tsx
+++ src/app/our-work/page.tsx
@@ -2,16 +2,14 @@ import ReadyCTASection from "@/components/homePage/ReadyCTASection";
 import HeroSection from "@/components/ourWorkPage/HeroSection";
 import ServicesSection from "@/components/ourWorkPage/ServicesSection";
 
-
 export default function page() {
   return (
     <main className="">
-        <HeroSection/>
-        {/* <section className="container mx-auto"> */}
-        <section className="bg-black">
-        <ServicesSection/>
-        </section>
-        <ReadyCTASection/>
+      <HeroSection />
+      <section className="bg-black">
+        <ServicesSection />
+      </section>
+      <ReadyCTASection />
     </main>
-  )
+  );
 }
