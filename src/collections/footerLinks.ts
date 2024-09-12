3d9ecb53b6e169e4d1341d178b2b755daf8d46ac
Michael Gitonga <gitoshmbae@gmail.com>
2024-09-12 13:22:16 +0300

    adds the footer component, adds lucide react icons

---
+++ src/collections/footerLinks.ts
@@ -0,0 +1,22 @@
+export const links=[
+    {
+        id:1,
+        name:'Home',
+        linkRef:'/',
+    },
+    {
+        id:2,
+        name:'About Us',
+        linkRef:'/about-us',
+    },
+    {
+        id:3,
+        name:'Services',
+        linkRef:'/our-work',
+    },
+    {
+        id:4,
+        name:'License',
+        linkRef:'#',
+    },
+]
