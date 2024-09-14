e4a686d48c9c71becb1178a0e561f4eeef07383d
Michael Gitonga <gitoshmbae@gmail.com>
2024-09-14 12:12:41 +0300

    adds an animation effect to the partner logos segements, adds a hover scale effect on the CTA buttons

--- src/components/aboutUsPage/PartnersSection.tsx
+++ src/components/aboutUsPage/PartnersSection.tsx
@@ -9,16 +9,32 @@ export default function PartnersSection() {
           <span className="text-green-400 uppercase">&#8226;</span>Partners
         </h2>
       </div>
-      <div className="flex flex-wrap gap-12 justify-center items-center  my-8">
-        {[1,2,3,4,5,6,7,8,9].map((item)=>(
+      <div className="flex space-x-12 overflow-hidden">
+        <div className="flex  space-x-12  motion-safe:animate-hortional-scroll-loop">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
             <div key={item} className="">
-                <p className="text-xl font-bold">Logoipsum</p>
+              <p className="text-xl font-bold">Logoipsum</p>
             </div>
-        ))}
+          ))}
+        </div>
+        <div
+          className="flex  space-x-12  motion-safe:animate-hortional-scroll-loop"
+          aria-hidden="true"
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+            <div key={item} className="">
+              <p className="text-xl font-bold">Logoipsum</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="bg-yellow-500 py-4 px-4 flex flex-wrap gap-4 justify-center items-center">
-        <p className="text-white text-center italic">We are always happy to collaborate with others for greater impact! </p>
-        <Button className="bg-white hover:bg-yellow-700 hover:text-white text-yellow-500 ">Become a Partner</Button>
+      <div className="bg-yellow-500 py-4 px-4 mt-12 flex flex-wrap gap-4 justify-center items-center">
+        <p className="text-white text-center italic">
+          We are always happy to collaborate with others for greater impact!{" "}
+        </p>
+        <Button className="bg-white hover:bg-yellow-700 hover:text-white text-yellow-500 transition ease-in hover:scale-105 ">
+          Become a Partner
+        </Button>
       </div>
     </section>
   );
