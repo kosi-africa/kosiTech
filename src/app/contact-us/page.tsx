c3b954f7225fb63e2e83ea74f1f64c0296914839
Michael Gitonga <gitoshmbae@gmail.com>
2024-09-14 16:58:04 +0300

    updates the ContactForm to be more reponsive across devices

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -2,31 +2,35 @@ import ContactForm from "@/components/forms/ContactForm";
 
 export default function page() {
   return (
-    <main className="min-h-screen w-screen  bg-black text-white p-28">
+    <main className="min-h-screen w-screen  bg-black text-white  lg:p-20 xl:p-48 ">
       <section className=" container mx-auto px-4 py-8 ">
-        <div className="grid grid-cols-12 gap-28">
-          <div className="col-span-12 lg:col-span-6 px-4 space-y-8">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-28">
+          <div className="w-3/6">
             <h1 className="text-3xl lg:text-5xl">Conatact Us</h1>
             {/* emaail & phone divs */}
-            <div className="flex flex-col md:flex-row justify-between gap-28 items-center">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-8 mt-8">
               <div className="space-y-4">
-                <p className="font-light text-gray-300 text-lg">Email</p>
-                <p className="text-gray-300 text-xl">team@kositech.co</p>
+                <p className="font-light text-gray-300 text-md">Email</p>
+                <p className="text-gray-300 text-lg lg:text-xl">
+                  team@kositech.co
+                </p>
               </div>
               <div className="space-y-4">
-                <p className="font-light text-gray-300 text-lg">Phone</p>
-                <p className="text-gray-300 text-xl">+254 712 817 268</p>
+                <p className="font-light text-gray-300 text-md">Phone</p>
+                <p className="text-gray-300 text-lg lg:text-xl">
+                  +254 712 817 268
+                </p>
               </div>
             </div>
             {/* address div  */}
-            <div className="space-y-4 pt-12 ">
-              <p className="font-light text-gray-300 text-lg">Address</p>
-              <p className="text-gray-300 text-xl">Nairobi Kenya</p>
+            <div className="space-y-4 pt-12 ml-8 md:ml-0 ">
+              <p className="font-light text-gray-300 text-md">Address</p>
+              <p className="text-gray-300 text-lg lg:text-xl">Nairobi Kenya</p>
             </div>
           </div>
           {/* form div */}
-          <div className="col-span-12 lg:col-span-6 space-y-8">
-            <ContactForm/>
+          <div className="w-3/6">
+            <ContactForm />
           </div>
         </div>
       </section>
