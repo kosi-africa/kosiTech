2778e497286911ae5d57fe0f893f593e472af91a
Michael Gitonga <gitoshmbae@gmail.com>
2024-10-24 16:12:45 +0300

    adds a new image reference in staticData/projectShowCase, updates the cards gradient color in components/homePage/AvailableSection, adds barasa email in actions/EmailNotification

--- src/actions/EmailNotification.ts
+++ src/actions/EmailNotification.ts
@@ -6,13 +6,12 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function EmailNotification(contactInforData:EmailTemplateProps) {
+export async function EmailNotification(contactInforData: EmailTemplateProps) {
   const { data, error } = await resend.emails.send({
     from: "Kosi Tech <partner@kosiafrica.org>",
-    to: ["michael@kosiafrica.org"],
+    to: ["barasa@kosiafrica.org"],
     subject: "Contact Notification",
     react: EmailTemplate(contactInforData),
-    //   react: EmailTemplate({ name: contactInforData.name }),
   });
   if (error) {
     console.log("RESEND:", error);
